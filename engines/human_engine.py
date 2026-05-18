b0ba592702fc23c8ccb69b6bbdc95c8a412d306d
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-18 19:14:39 +0700

    Physical Environment System [step 1]

--- engines/human_engine.py
+++ engines/human_engine.py
@@ -4,6 +4,28 @@ class HumanEngine:
 
         hour = world.time["hour"]
 
+        # Tentukan activity berdasarkan jam
+        if 0 <= hour < 6:
+            activity = "Sleeping"
+            occupancy = 1
+
+        elif 6 <= hour < 8:
+            activity = "Morning Routine"
+            occupancy = 1
+
+        elif 8 <= hour < 17:
+            activity = "Outside Working"
+            occupancy = 0
+
+        elif 17 <= hour < 22:
+            activity = "Gaming"
+            occupancy = 1
+
+        else:
+            activity = "Relaxing"
+            occupancy = 1
+
+        # Movement intensity
         movement = 0
 
         if activity == "Sleeping":
@@ -18,6 +40,10 @@ class HumanEngine:
         elif activity == "Relaxing":
             movement = 8
 
+        elif activity == "Outside Working":
+            movement = 0
+
+        # Simpan ke world state
         world.human = {
             "activity": activity,
             "occupancy": occupancy,

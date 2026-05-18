01f81668a5c56dbb314a463b3d08b8f7ec0b9839
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-18 23:10:13 +0700

    virtual timeline system - Update v1.1

--- core/tick_engine.py
+++ core/tick_engine.py
@@ -1,18 +1,10 @@
-from datetime import timedelta
-
 class TickEngine:
+
     def __init__(self):
         self.tick = 0
 
     def next_tick(self, world):
+
         self.tick += 1
 
-        world.time["timestamp"] += timedelta(minutes=1)
-
-        current = world.time["timestamp"]
-
-        world.time["hour"] = current.hour
-        world.time["minute"] = current.minute
-        world.time["weekday"] = current.strftime("%A")
-
-        print(f"[TICK {self.tick}] {current}")
+        print(f"[TICK {self.tick}]")

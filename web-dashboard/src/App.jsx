da71a533bcdce05160a15eb9ea67ddec181f3ba8
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-19 05:25:56 +0700

    Initialize React dashboard foundation

--- web-dashboard/src/App.jsx
+++ web-dashboard/src/App.jsx
@@ -1,36 +1,5 @@
-import DashboardLayout from "./layouts/DashboardLayout"
-import Sidebar from "./components/Sidebar"
-import Topbar from "./components/Topbar"
+import LiveRoom from "./pages/LiveRoom"
 
-function App() {
-  return (
-    <DashboardLayout>
-      <Sidebar />
-
-      <div
-        style={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Topbar />
-
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "#94a3b8",
-            fontSize: "2rem",
-          }}
-        >
-          Live Room Visualization Area
-        </div>
-      </div>
-    </DashboardLayout>
-  )
+export default function App() {
+  return <LiveRoom />
 }
-
-export default App

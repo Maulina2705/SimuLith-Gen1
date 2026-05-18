82794baf2d2d9a68c07954314300460ec2696849
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-18 21:56:46 +0700

    Update logic v1.1

--- engines/export_engine.py
+++ engines/export_engine.py
@@ -13,22 +13,60 @@ class ExportEngine:
 
             # TIME
             "timestamp": world.time["timestamp"],
+            "hour": world.time["hour"],
+            "minute": world.time["minute"],
+            "weekday": world.time["weekday"],
 
             # WEATHER
             "weather_type": world.weather["weather_type"],
             "outside_temp": world.weather["outside_temp"],
+            "outside_humidity": world.weather["outside_humidity"],
+            "weather_remaining": world.weather["weather_remaining"],
+
+            # EVENTS
+            "active_event": world.events["active_event"],
+            "event_remaining": world.events["event_remaining"],
 
             # HUMAN
             "activity": world.human["activity"],
             "occupancy": world.human["occupancy"],
             "movement_intensity": world.human["movement_intensity"],
+            "activity_remaining": world.human["activity_remaining"],
 
             # ENVIRONMENT
             "inside_temp": world.environment["inside_temp"],
             "inside_humidity": world.environment["inside_humidity"],
+            "indoor_brightness": world.environment["indoor_brightness"],
+
+            # AC
+            "ac_state": world.devices["ac"]["state"],
+            "ac_power_w": world.devices["ac"]["power_w"],
+            "ac_target_temp": world.devices["ac"]["target_temp"],
+
+            # PC
+            "pc_state": world.devices["pc"]["state"],
+            "pc_power_w": world.devices["pc"]["power_w"],
+
+            # MAIN LAMP
+            "main_lamp_state": world.devices["main_lamp"]["state"],
+            "main_lamp_power_w": world.devices["main_lamp"]["power_w"],
+
+            # DESK LAMP
+            "desk_lamp_state": world.devices["desk_lamp"]["state"],
+            "desk_lamp_power_w": world.devices["desk_lamp"]["power_w"],
+
+            # RICE COOKER
+            "rice_cooker_state": world.devices["rice_cooker"]["state"],
+            "rice_cooker_power_w": world.devices["rice_cooker"]["power_w"],
+
+            # WASHING MACHINE
+            "washing_machine_state": world.devices["washing_machine"]["state"],
+            "washing_machine_power_w": world.devices["washing_machine"]["power_w"],
 
             # ENERGY
             "total_power_w": world.energy["total_power_w"],
+            "total_current_a": world.energy["total_current_a"],
+            "voltage_v": world.energy["voltage_v"],
             "total_energy_kwh": world.energy["total_energy_kwh"],
 
             # SENSOR
